8647e903e8bd601035d10e9c8bddd8370668685b
JustASpoody <92946924+JustASpoody@users.noreply.github.com>
2022-02-01 16:47:09 +0500

    Update AWEB.py

--- AWEB.py
+++ AWEB.py
@@ -1,6 +1,6 @@
 def spn(toponym):
     bounded_by = toponym["boundedBy"]["Envelope"]
-    upper_corner = bounded_by["upperCorner"].split(" ")  # 37.530887 55.703118
+    upper_corner = bounded_by["upperCorner"].split(" ")
     lower_corner = bounded_by["lowerCorner"].split(" ")
     dlon = float(upper_corner[0]) - float(lower_corner[0])
     dlat = float(upper_corner[1]) - float(lower_corner[1])
